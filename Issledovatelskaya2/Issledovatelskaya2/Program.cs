00bdd0cb4d814eec68eeac5cad81a464718eaae5
V Andrey <av@gpcom.ru>
2019-01-20 18:25:00 +0500

    doble

--- Issledovatelskaya2/Issledovatelskaya2/Program.cs
+++ Issledovatelskaya2/Issledovatelskaya2/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 //using static System.Console;
-using Extreme.Mathematics;
 using System.IO;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
@@ -14,32 +13,32 @@ namespace Issledovatelskaya2
 {
     public class stageClass
     {
-        public Decimal isp;
-        public Decimal specificThrust;
-        public Decimal massStart;
-        public Decimal massFin;
-        public Decimal deltaV;
-        public Decimal thrust100;
-        public Decimal timeToWork;
-        public Decimal TWRstart;
-        public Decimal TWRfin;
-        public Decimal massflow100;
+        public Double isp;
+        public Double specificThrust;
+        public Double massStart;
+        public Double massFin;
+        public Double deltaV;
+        public Double thrust100;
+        public Double timeToWork;
+        public Double TWRstart;
+        public Double TWRfin;
+        public Double massflow100;
     }
     class Program
     {
         static void Main(string[] args)
         {
-            const Decimal shag = 1M;
-            Decimal time, timePred;
-            Decimal x, xPred, y, yPred, vel, velPred, velX, velXPred, velY, velYPred, angleVelRad, gEar, gEarX, gEarY, angleGEarthRad, gMoo, gMooX, gMooY, angleGMooRad;
-            Decimal accel, accelX, accelY, angleAccelRad, angleAccelDegr, thrustPercent, thrust, massflow, mass, massPred, timeBeforeBurnOut, TWR, fuelRemainPercent, deltaVel;
-            Decimal heightEar, soundSpeed, airDensity, Cx, airForce, machNumber, Cx_withoutK, sectional;
-            Decimal moonX, moonY, moonVX, moonVY, moonV, moonAngleV, moonGfromEar, moonGfromEarX, moonGfromEarY, moonXPred, moonYPred, moonVXPred, moonVYPred, moonGfromEarXPred, moonGfromEarYPred, moonAngleG;
+            const Double shag = 1;
+            Double time, timePred;
+            Double x, xPred, y, yPred, vel, velPred, velX, velXPred, velY, velYPred, angleVelRad, gEar, gEarX, gEarY, angleGEarthRad, gMoo, gMooX, gMooY, angleGMooRad;
+            Double accel, accelX, accelY, angleAccelRad, angleAccelDegr, thrustPercent, thrust, massflow, mass, massPred, timeBeforeBurnOut, TWR, fuelRemainPercent, deltaVel;
+            Double heightEar, soundSpeed, airDensity, Cx, airForce, machNumber, Cx_withoutK, sectional;
+            Double moonX, moonY, moonVX, moonVY, moonV, moonAngleV, moonGfromEar, moonGfromEarX, moonGfromEarY, moonXPred, moonYPred, moonVXPred, moonVYPred, moonGfromEarXPred, moonGfromEarYPred, moonAngleG;
             int stageNumber, stageKolvo;
-            const Decimal massEarthG = 398600441580000;//гравитационный параметр
-            const Decimal massMoonG = 4902800030000;
-            const Decimal radiusEarth = 6378100;
-            const Decimal radiusMoon = 1738140;
+            const Double massEarthG = 398600441580000;//гравитационный параметр
+            const Double massMoonG = 4902800030000;
+            const Double radiusEarth = 6378100;
+            const Double radiusMoon = 1738140;
             stageClass[] stage = new stageClass[1];
             bool trigStageExit = false;
             //StreamWriter writer = new StreamWriter(@"D:\Advanced Grapher\Untitled2.txt");
@@ -61,13 +60,13 @@ namespace Issledovatelskaya2
 
 
                     Console.WriteLine("Характеристики ступени " + (kolvo + 1));
-                    Console.Write("Удельная тяга, сек = "); stage[kolvo].specificThrust = Decimal.Parse(Console.ReadLine());
-                    Console.Write("Wet mass, кг = "); stage[kolvo].massStart = Decimal.Parse(Console.ReadLine());
-                    Console.Write("Dry mass, кг = "); stage[kolvo].massFin = Decimal.Parse(Console.ReadLine());
-                    Console.Write("Время работы при 100% тяги, сек = "); stage[kolvo].timeToWork = Decimal.Parse(Console.ReadLine());
+                    Console.Write("Удельная тяга, сек = "); stage[kolvo].specificThrust = Double.Parse(Console.ReadLine());
+                    Console.Write("Wet mass, кг = "); stage[kolvo].massStart = Double.Parse(Console.ReadLine());
+                    Console.Write("Dry mass, кг = "); stage[kolvo].massFin = Double.Parse(Console.ReadLine());
+                    Console.Write("Время работы при 100% тяги, сек = "); stage[kolvo].timeToWork = Double.Parse(Console.ReadLine());
 
                     stage[kolvo].isp = stage[kolvo].specificThrust * 9.8M;
-                    stage[kolvo].deltaV = stage[kolvo].isp * Extreme.Mathematics.DecimalMath.Log(stage[kolvo].massStart / stage[kolvo].massFin);
+                    stage[kolvo].deltaV = stage[kolvo].isp * Extreme.Mathematics.DoubleMath.Log(stage[kolvo].massStart / stage[kolvo].massFin);
                     stage[kolvo].massflow100 = (stage[kolvo].massStart - stage[kolvo].massFin) / stage[kolvo].timeToWork;
                     stage[kolvo].thrust100 = stage[kolvo].isp * stage[kolvo].massflow100;
                     stage[kolvo].TWRstart = stage[kolvo].thrust100 / (stage[kolvo].massStart * 9.8M);
@@ -105,9 +104,9 @@ namespace Issledovatelskaya2
             */
             timePred = 0; time = 0;
             moonX = 0; moonY = 405696000;
-            moonAngleV = DecimalMath.Pi * 1.5M;//Начальные значения
+            moonAngleV = Math.PI * 1.5;//Начальные значения
             //moonV = 963.38744M;
-            moonVXPred = -963.368300825M; moonVYPred = 0;
+            moonVXPred = -963.368300825; moonVYPred = 0;
             moonGfromEarXPred = 0; moonGfromEarYPred = 0;
             moonXPred = 0; moonYPred = 405696000;
             moonVX = 0; moonVY = 0;
@@ -123,9 +122,9 @@ namespace Issledovatelskaya2
                     moonX = moonXPred + (moonVX * shag);
                     moonY = moonYPred + (moonVY * shag);
                     moonGfromEar = massEarthG /
-                        (DecimalMath.Pow(moonX, 2) + DecimalMath.Pow(moonY, 2));
+                        (Math.Pow(moonX, 2) + Math.Pow(moonY, 2));
 
-                    Decimal k = new decimal();
+                    Double k = new Double();
                     if (moonY > 0)
                     {
                         k = 1;
@@ -138,12 +137,12 @@ namespace Issledovatelskaya2
                     {
                         k = 0;
                     }
-                    moonAngleG = DecimalMath.Atan(moonX / moonY) + DecimalMath.Pi * k;
-                    moonGfromEarX = moonGfromEar * DecimalMath.Sin(moonAngleG);
-                    moonGfromEarY = moonGfromEar * DecimalMath.Cos(moonAngleG);
+                    moonAngleG = Math.Atan(moonX / moonY) + Math.Pi * k;
+                    moonGfromEarX = moonGfromEar * Math.Sin(moonAngleG);
+                    moonGfromEarY = moonGfromEar * Math.Cos(moonAngleG);
 
                     if (moonVX < 0) { k = 1.5M; } else { k = 0.5M; }
-                    //moonAngleV = -1 * DecimalMath.Atan(moonVY / moonVX) + (DecimalMath.Pi * k);
+                    //moonAngleV = -1 * Math.Atan(moonVY / moonVX) + (Math.Pi * k);
 
 
 
